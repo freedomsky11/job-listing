14db28a153a2cde3412749de539d362dcebb0451
freedomsky <hanwei_1989@126.com>
2017-01-21 22:10:12 +0800

    add resume and hook job/user relation

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,16 +1,15 @@
 class ResumesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+
 
   def index
-    @jobs = Job.all
   end
 
   def new
-    @job = Job.find(params[:job_id])
   end
 
   def create
-    @job = Job.find(params[:job_id])
   end
 
+  def destroy
+  end
 end
