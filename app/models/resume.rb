f6a3c51c66dac5579b1a57e103ef102650a9dd56
freedomsky <hanwei_1989@126.com>
2017-01-21 23:23:50 +0800

    finish step7 byself

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,6 @@
-class Resume < ApplicationRecord
+class Resume < ActiveRecord::Base
+  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+  validates :job_id, presence: true # Make sure the owner's name is present.
   belongs_to :user
   belongs_to :job
   validates :content, presence: true
