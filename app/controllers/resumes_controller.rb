0d922afdc68d9f8617d8e79b32565a3064c6ad47
freedomsky <hanwei_1989@126.com>
2017-01-21 22:41:33 +0800

    add page resume#new

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,15 +1,33 @@
 class ResumesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
   end
 
   def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
   end
 
   def create
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new(resume_params)
+    @resume.job = @job
+    @resume.user = current_user
+
+    if @post.save
+      redirect_to job_path(@job)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:content, :job_id, :user_id)
   end
 end
