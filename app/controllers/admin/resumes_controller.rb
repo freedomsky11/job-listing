fa47606aef3d27dc00b428a485f3de23ee459a0d
freedomsky <hanwei_1989@126.com>
2017-01-22 13:07:43 +0800

    程序优化完成。

--- app/controllers/admin/resumes_controller.rb
+++ app/controllers/admin/resumes_controller.rb
@@ -5,8 +5,8 @@ class Admin::ResumesController < ApplicationController
   layout 'admin'
 
   def index
-    @job = Job.find(params[:id])
+    @job = Job.find(params[:job_id])
     @resumes = @job.resumes.order('created_at DESC')
   end
-  
+
 end
