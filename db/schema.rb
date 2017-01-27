61b7f135dd5fff6c8318fe4a0034b3952b523f8c
freedomsky <hanwei_1989@126.com>
2017-01-28 02:07:21 +0800

    MG v5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121144945) do
+ActiveRecord::Schema.define(version: 20170127124551) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20170121144945) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: false
+    t.string   "city"
+    t.string   "company"
+    t.string   "category"
   end
 
   create_table "resumes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,81 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
+puts "這個種子檔會自動建立一個admin帳號, 並且創建一些jobs"
 
-create_account = User.create([email: '1@qq.com', password: '11111111', password_confirmation: '11111111', is_admin: 'true'])
+create_account = User.create([email: '1@qq.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
 puts "Admin account created."
 
-create_jos = for i in 1..10 do
-  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
-end
-puts "10 Public jobs created."
+jobs = [
+  "api程序员",
+  "iOS开发工程师",
+  "Html5工程师",
+  "Android开发工程师",
+  "测试工程师",
+  "rails工程师",
+  "法律顾问",
+  "人力资源主管",
+  "UI设计师",
+  "行政主管",
+  "平面设计师",
+  "工业设计师",
+]
+category = [
+  "技术",
+  "运营",
+  "市场与销售",
+  "职能",
+  "金融",
+  "法律",
+  "人力资源",
+  "企业管理",
+]
+company = [
+  "小米",
+  "百度",
+  "阿里巴巴",
+  "腾讯",
+  "锥子科技",
+  "京东",
+]
+city = [
+  "北京",
+  "上海",
+  "广州",
+  "深圳",
+  "杭州",
+  "厦门",
+]
 
-create_jos = for i in 1..10 do
-  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+create_jos = for i in 1..30 do
+  Job.create!([
+    title: jobs[rand(jobs.length)],
+    # title: "JOB.no#{i}",
+    description: "这是用种子建立的第 #{i} 个Public工作哦",
+    wage_upper_bound: rand(50..99)*100 + 5000,
+    wage_lower_bound: rand(10..49)*100 + 5000,
+    contact_email: 'text@gmail.com',
+    is_hidden: "false",
+    city: city[rand(city.length)],
+    company: company[rand(company.length)],
+    category: category[rand(category.length)]
+  ])
 end
-puts "10 Hidden jobs created."
+
+puts "30 Public jobs created."
+
+create_jos = for i in 1..30 do
+  Job.create!([
+    title: jobs[rand(jobs.length)],
+    # title: "JOB.no#{i}",
+    description: "这是用种子建立的第 #{i+10} 个hidden工作哦",
+    wage_upper_bound: rand(50..99)*100 + 5000,
+    wage_lower_bound: rand(10..49)*100 + 5000,
+    contact_email: 'text@gmail.com',
+    is_hidden: "true",
+    city: city[rand(city.length)],
+    company: company[rand(company.length)],
+    category: category[rand(category.length)]
+  ])
+end
+puts "30 Hidden jobs created."
