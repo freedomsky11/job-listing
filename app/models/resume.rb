fa213a1d99e8f7cc9d3f631dd7cf0f2fa0dc75f9
freedomsky <hanwei_1989@126.com>
2017-07-21 22:06:13 +0800

    admin can see resume list

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,8 +1,8 @@
 class Resume < ApplicationRecord
-  validates :job_id, presence: true
-  validates :user_id, presence: true
-  validates :attachment, presence: true
-  validates :content, presence: true  
+  # validates :job_id, presence: true
+  # validates :user_id, presence: true
+  # validates :attachment, presence: true
+  validates :content, presence: true
   mount_uploader :attachment, AttachmentUploader # Tells rials to use this uploader for this model.
 
   belongs_to :job
